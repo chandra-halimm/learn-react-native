20c2eecf659bdf2f5c20719a5bb8830e27f4f94a
chandra-halimm <chan.halim98@gmail.com>
2025-11-03 01:53:54 +0700

    f

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,49 @@
 import { Stack, useRouter } from "expo-router";
-import { useEffect } from "react";
-
-function RouteGuard({children}: {children: React.ReactNode}) {
-    const route = useRouter();
-    const isAuthenticated = false;
-
-    useEffect(() => {
-        
-        if (!isAuthenticated) {
-            route.replace("/auth");
-        }
-    });
-
-    return <>{children}</>;
-}
+import { useEffect, useState } from "react";
+import { Text } from "react-native"; // Atau gunakan Splash Screen
 
 export default function RootLayout() {
+  const router = useRouter();
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      // const token = await AsyncStorage.getItem('token');
+      // if (token) {
+      //   setIsAuthenticated(true);
+      // } else {
+      //   setIsAuthenticated(false);
+      // }
+      
+      setIsAuthenticated(false); 
+      setIsLoading(false); 
+    };
+
+    checkAuth();
+  }, []); 
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (!isAuthenticated) {
+      router.replace("/auth");
+    }  else {
+        router.replace("/login");
+    }
+  }, [isLoading, isAuthenticated, router]); 
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-    <RouteGuard>
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-    </RouteGuard>
-  )
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="auth" options={{ headerShown: false }} /> 
+    </Stack>
+  );
 }
--- app/auth.tsx
+++ app/auth.tsx
@@ -6,13 +6,13 @@ export default function AuthScreen() {
   const [isSignUp, setIsSignUp] = useState(false);
   return (
   <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"}>
-    <View>
+    <View style={{padding: 20, marginTop: 100}}>
       <Text>{isSignUp ? "Create Account": "Welcome Back"}</Text>
       <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="exmaple@gmail.com" mode="outlined"/>
 
-      <TextInput label="Password" mode="outlined"/>
+      <TextInput style={{marginTop: 20}} label="Password" mode="outlined"/>
 
-      <Button mode="contained">{isSignUp ? "Sign Up" : "Sign In"}</Button>
+      <Button mode="contained" style={{marginTop: 20}}>{isSignUp ? "Sign Up" : "Sign In"}</Button>
       <Button mode="text">{ isSignUp ? "Already haven an account? Sign In" : "Don't have an account? Sign Up" }</Button>
     </View>
   </KeyboardAvoidingView>
